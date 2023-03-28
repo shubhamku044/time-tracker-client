579be0623238e73c3c4e3a0473763f6637325679
= Sital Adhikari <sitaladhikari002@gmail.com>
2023-03-28 21:40:23 +0545

    Added icons

--- src/components/side-panel/index.tsx
+++ src/components/side-panel/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { SidePanelCon, SidePanelEl } from './styled';
 import { NavLink } from 'react-router-dom';
-
+import {BiTime} from 'react-icons/bi'
+import {VscProject} from 'react-icons/vsc'
 const SidePanel = () => {
   return (
     <SidePanelCon>
       <SidePanelEl>
         <NavLink to="/">
-        Time tracker
+          <BiTime/>
+        <span>Time Tracker</span>
         </NavLink>
         <NavLink to="/projects">
-        Projects
+        <VscProject/>
+        <span>Projects</span>
         </NavLink>
       </SidePanelEl>
     </SidePanelCon>
