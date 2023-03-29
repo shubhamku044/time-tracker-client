63878025ed1a729827c7106beddecc8887fd823c
Aryan Kumar <aryankumar877@gmail.com>
2023-03-29 05:34:57 +0530

    added sections to sidebar

--- src/components/side-panel/index.tsx
+++ src/components/side-panel/index.tsx
@@ -1,23 +1,73 @@
+
 import React from 'react';
-import { SidePanelCon, SidePanelEl } from './styled';
+import { BiBarChartSquare, BiCalendar, BiTime } from 'react-icons/bi';
+import { BsCardList, BsFileBarGraph, BsTag } from 'react-icons/bs';
+import { CiViewTimeline } from 'react-icons/ci';
+import { FaCog } from 'react-icons/fa';
+import { IoMdPeople } from 'react-icons/io';
+import { MdFace, MdSchedule } from 'react-icons/md';
+import { RxDashboard } from 'react-icons/rx';
+import { VscProject } from 'react-icons/vsc';
 import { NavLink } from 'react-router-dom';
-import {BiTime} from 'react-icons/bi'
-import {VscProject} from 'react-icons/vsc'
+import { HeadingText, LinkText, SidePanelCon, SidePanelEl } from './styled';
+'react-icons/md';
+'react-icons/ci';
+
 const SidePanel = () => {
   return (
     <SidePanelCon>
       <SidePanelEl>
         <NavLink to="/">
-          <BiTime/>
-        <span>Time Tracker</span>
+          <CiViewTimeline size={32} color="#9CA3AF" />
+          <LinkText>TIMESHEET</LinkText>
         </NavLink>
+        <NavLink to="/time-tracker">
+          <BiTime size={28} color="#9CA3AF" />
+          <LinkText>TIME TRACKER</LinkText>
+        </NavLink>
+        <NavLink to="/calendar">
+          <BiCalendar size={28} color="#9CA3AF" />
+          <LinkText>CALENDAR</LinkText>
+        </NavLink>
+      </SidePanelEl>
+      <HeadingText>ANALYZE</HeadingText>
+      <SidePanelEl>
+        <NavLink to="/dashboard">
+          <RxDashboard size={28} color="#9CA3AF" />
+          <LinkText>DASHBOARD</LinkText>
+        </NavLink>
+        <NavLink to="/reports">
+          <BsFileBarGraph size={28} color="#9CA3AF" />
+          <LinkText>REPORTS</LinkText>
+        </NavLink>
+      </SidePanelEl>
+      <HeadingText>MANAGE</HeadingText>
+      <SidePanelEl>
         <NavLink to="/projects">
-        <VscProject/>
-        <span>Projects</span>
+          <BsCardList size={28} color="#9CA3AF" />
+          <LinkText>PROJECTS</LinkText>
+        </NavLink>
+        <NavLink to="/tags">
+          <BsTag size={28} color="#9CA3AF" />
+          <LinkText>TAGS</LinkText>
+        </NavLink>
+        <NavLink to="/team">
+          <IoMdPeople size={28} color="#9CA3AF" />
+          <LinkText>TEAM</LinkText>
+        </NavLink>
+        <NavLink to="/clients">
+          <MdFace size={28} color="#9CA3AF" />
+          <LinkText>CLIENTS</LinkText>
+        </NavLink>
+        <NavLink to="/settings">
+          <FaCog size={28} color="#9CA3AF" />
+          <LinkText>SETTINGS</LinkText>
         </NavLink>
       </SidePanelEl>
     </SidePanelCon>
+
   );
 };
 
 export default SidePanel;
+
--- src/components/side-panel/styled.ts
+++ src/components/side-panel/styled.ts
@@ -2,29 +2,55 @@ import styled from 'styled-components';
 
 export const SidePanelCon = styled.div`
   width: 14rem;
-  background-color: ${({theme}) => theme.colors.header};
-  min-height: calc(100vh - 40px);
+  background-color: ${({ theme }) => theme.colors.header};
   position: fixed;
+  padding-top: 16px;
+  padding-bottom: 16px;
   top: 40px;
+  bottom: 0;
+  overflow-y: auto;
+`;
+
+export const LinkText = styled.span`
+  color: #9ca3af;
+  margin-left: 0px;
+  margin-top: 8px;
+  margin-bottom: 8px;
+  font-weight: 300;
+  display: flex;
+  align-items: center;
+`;
+
+export const HeadingText = styled.div`
+  display: flex;
+  align-items: center;
+  color: #9ca3af;
+  font-weight: bold;
+  margin-bottom: 0px;
+  gap: 10px;
+  padding: 1rem 1.2rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
 `;
 
 export const SidePanelEl = styled.div`
   display: flex;
   flex-direction: column;
+  overflow-y: scroll;
 
   & a {
     display: flex;
-    gap: 10px;
     align-items: center;
-    font-size: 1.2rem;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    padding: 1rem 1.2rem;
+    padding: 0.6rem 1.2rem;
     color: inherit;
-    transition: all .2s ease-out;
+    transition: all 0.2s ease-out;
 
     &:hover {
       background-color: ${({ theme }) => theme.colors.background};
+    }
+
+    & svg {
+      margin-right: 16px;
     }
   }
 `;
